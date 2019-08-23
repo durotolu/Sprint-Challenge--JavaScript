90a7238a906aa223f744a6f8537c67445953711b
Modurotolu Olokode <durolawk@gmail.com>
2019-08-23 08:51:08 +0100

    console.logged the answers

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -39,16 +39,16 @@ let vraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tsaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(vraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(ssaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tsaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
